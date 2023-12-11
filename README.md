422beb945b4d4a3fc515965854398d6a14b12550
uzairansari11 <uzairans532@gmail.com>
2023-12-11 17:56:55 +0530

    react query basic

--- README.md
+++ README.md
@@ -1,4 +1,93 @@
-## Important points
-* default cache time : 5 minutes,
-* default stale time : 0  sec 
+### syntax
 
+```
+const resp = useQuery ('keyName' , fetch_function,
+          {
+        options | different config
+            })
+```
+
+## Important points
+
+- default cache time : 5 minutes,
+- default stale time : 0 sec
+- refetchOnMount : true | false | 'always'
+- refetchOnWindowFocus: true | false | 'always'
+
+## pooling
+
+- refetchInterval : false by default take value in second,
+- refetchIntervalInBackground : true this flag let data fetch even window loses the focus,
+
+## fetch on click
+
+- when a component mount react-query automatic
+  fires the fetch function but what if we want
+  it on a click event
+
+- step 1  
+  setting up {enable : false}
+
+- step 2
+  useQuery give a function called 'refetch'
+  which we can invoke onClick
+
+## success | error callbacks
+
+if you want to perform any side effect after success or error api call status you can pass onSuccess | onError key attaching with function which have access to data and error if we pass the argument
+
+```
+const onSuccess = (data)=>{
+    // WE CAN PERFORM SIDE EFFECTS
+    console.log(data)
+}
+
+const onError = (error)=>{
+    console.log(error)
+}
+
+const res = useQuery(
+    'key name',
+    ()=>{
+        return axios.get(url),
+        {
+            onSuccess,
+            onError
+        }
+    }
+)
+
+```
+
+### data transformation
+
+- React query also provide a function called select using which we can transform our data
+  success has access to our response
+
+```
+const onSuccess = (data)=>{
+   // WE CAN PERFORM SIDE EFFECTS
+   console.log(data)
+}
+
+const onError = (error)=>{
+   console.log(error)
+}
+
+const res = useQuery(
+   'key name',
+   ()=>{
+       return axios.get(url),
+       {
+           onSuccess,
+           onError,
+           success : (data)=>{
+           const name = data.data.map((ele)=>{
+            return ele.name 
+            })
+            
+           }
+       }
+   }
+)
+```
--- src/components/RQFsuperhero.js
+++ src/components/RQFsuperhero.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
 const RQFsuperhero = () => {
-  const { data, isLoading, isError, error } = useQuery(
+  const [pooling, setPooling] = useState(3000);
+
+  const onSuccess = (data) => {
+    // console.log(data,"i am data")
+    // console.log(" i am inside onSuccess function ");
+    if (data?.length === 4) {
+      setPooling(false);
+    }
+  };
+  const onError = (error) => {
+    setPooling(false);
+  };
+  const { data, isLoading, isError, error, refetch } = useQuery(
     "super-hero-data",
     () => axios.get("http://localhost:8080/superheros"),
     {
-      cacheTime: 10000, 
-      staleTime:300000,
+      // cacheTime: 10000,
+      // staleTime: 300000,
+      // enabled: false,
+      refetchOnMount: true,
+      refetchOnWindowFocus: "always",
+      refetchInterval: pooling,
+      refetchIntervalInBackground: true,
+      onSuccess,
+      onError,
+      select: (data) =>  {
+        const superheroName = data.data.map((ele) => ele.name)
+        console.log(superheroName,"superhero name")
+        return superheroName
+      }
     }
   );
 
@@ -17,13 +41,28 @@ const RQFsuperhero = () => {
       <p style={{ fontWeight: "bolder", fontSize: "30px", color: "red" }}>
         Superheros List RQF
       </p>
+      <button
+        style={{
+          color: "white",
+          background: "red",
+          padding: "0.5rem 2rem",
+          borderRadius: "0.2rem",
+          border: "none",
+        }}
+        onClick={refetch}
+      >
+        Fetch Superheroes Data
+      </button>
       {isLoading ? (
         <h3>Loading the data ....</h3>
       ) : isError ? (
         <h3>{error.message}</h3>
       ) : (
-        data?.data.length &&
-        data.data.map((ele) => <p key={ele.id}>{ele.name}</p>)
+        // data?.data.length &&
+        // data.data.map((ele) => <p key={ele.id}>{ele.name}</p>)
+
+        data?.length &&
+        data.map((ele) => <p key={ele}>{ele}</p>)
       )}
     </div>
   );
